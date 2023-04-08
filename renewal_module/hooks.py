38576dde39ef7a5ab8fc2fcc90a1247783d89aa7
jhansi.a64@gmail.com <jhansi.a64@gmail.com>
2023-04-08 12:59:32 +0000

    rem quot

--- renewal_module/hooks.py
+++ renewal_module/hooks.py
@@ -112,9 +112,9 @@ fixtures = [{"dt": "Custom Field", "filters": [["name", "in", [
 # DocType Class
 # ---------------
 # Override standard doctype classes
-override_doctype_class = {
-	"Quotation": "renewal_module.custom_doc.custom_quo.Custom_quotation"
-}
+#override_doctype_class = {
+#	"Quotation": "renewal_module.custom_doc.custom_quo.Custom_quotation"
+#}
 # override_doctype_class = {
 # 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
