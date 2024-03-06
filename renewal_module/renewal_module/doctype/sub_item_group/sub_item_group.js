3d95ab55d3617eda8d37b96eec558cd45c13669e
Ecaps64 <dev.ecaps@gmail.com>
2024-03-06 07:28:23 +0000

    060324

---
+++ renewal_module/renewal_module/doctype/sub_item_group/sub_item_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Aravind Mandala and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sub Item Group', {
+	// refresh: function(frm) {
+
+	// }
+});
